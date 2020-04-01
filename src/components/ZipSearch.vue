80979473e701cc4a2da0d3585fb86642a5d3ca1e
ahmadfathy97 <af1381@fayoum.edu.eg>
2020-04-01 17:56:45 +0200

    upload files

---
+++ src/components/ZipSearch.vue
@@ -0,0 +1,47 @@
+<template id="">
+  <ion-grid>
+    <form @submit="onSubmit">
+      <ion-col>
+        <ion-item>
+          <ion-label>zipcode</ion-label>
+          <ion-input :value="zip" @input="zip = $event.target.value" name="zip"></ion-input>
+        </ion-item>
+      </ion-col>
+      <ion-col>
+        <ion-button type="submit" color="primary" expand="block">Find</ion-button>
+      </ion-col>
+    </form>
+  </ion-grid>
+</template>
+<script>
+export default{
+  name: 'ZipSeaech',
+  data(){
+    return{
+      zip: ''
+    }
+  },
+  methods:{
+    onSubmit(e){
+      e.preventDefault();
+      const isValidZip = /(^\d{5}$)|(^\d{5}-\d{4}$)/.test(this.zip);
+      if (!isValidZip) {
+        this.showAlert();
+        this.zip = "";
+      } else {
+        this.$emit("get-zip", this.zip);
+        this.zip = "";
+      }
+    },
+    showAlert() {
+      return this.$ionic.alertController
+        .create({
+          header: "Not valid Zipcode",
+          message: "Please enter a valid US zipcode",
+          buttons: ["OK"]
+        })
+        .then(a => a.present());
+    }
+  }
+}
+</script>
